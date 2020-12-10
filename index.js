1d0cd776d104dc8d9a7ba3372e47740219cdac74
Julio Cesar Fernandez Hernandez <cesarfdez_-_@hotmail.com>
2020-12-10 17:45:48 -0600

    En construccion ejemplo

--- index.js
+++ index.js
@@ -3,21 +3,24 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html');
-// });
-
 io.on('connection', (socket) => {
   //console.log('a user connected', socket.id);
-
+ 
   socket.on('disconnect', () => {
     //console.log('user disconnected', socket.id);
   });
-
+  //Metodo de envio de mensajes del Ejemplo 
   socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
     io.emit('chat message', msg);
   });
+  
+
+  socket.on('chatExample', (msg) => {
+
+      io.emit('chatExample', '' + msg);
+    
+  });
+
 });
 
 app.use(express.static('public'));
